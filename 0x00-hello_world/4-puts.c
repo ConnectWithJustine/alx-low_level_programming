fe9b511ade09de23db56895f4c4e170cf6a5b6ae
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-02-16 13:49:05 -0800

    adding puts script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multiligual puzzle\n");
+	return (0);
+}
