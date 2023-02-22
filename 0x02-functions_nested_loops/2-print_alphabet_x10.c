304dd21ba64fecb3cd17b05d72e29e002da33920
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-02-21 17:12:58 -0800

    adding alphabet 10x file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - a function that prints 10 times the alphabet, in lowercase
+ *
+ * Return: Always 0 (succes)
+ */
+void print_alphabet_x10(void)
+{
+	int i = a, co;
+	
+	co =0;
+
+	while (co <10)
+	{
+	
+	for (i<=z)
+		i++;
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	co++;
+	}
+}
