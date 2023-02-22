a3dae60466aaced7a6403ed38587c0c94766c441
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-02-21 19:44:28 -0800

    advance task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_times_table - Entry point
+ * @i: input
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int i)
+{
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mainh"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	return (0);
+}
