85c986f446ac0670af0ca12d75c260003dc04bba
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-02-21 18:46:27 -0800

    value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the abolute value of an integer
+ * @ab: integer
+ * Return: Always 0 (succes0
+ */
+int _abs(int ab)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
+
