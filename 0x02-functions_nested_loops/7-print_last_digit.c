97224d0848e11eaf9c2e4099e3474a5c129ca4a9
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-02-21 18:59:05 -0800

    last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * prin_last_digit - a function that print last digit of a number
+ *@ild: last digit result
+ * Return: value of last digi
+ */
+int print_last_digit(int ild)
+{
+	int pld;
+
+	pld = (ild % 10);
+
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
+	_putchar(pld + '0');
+	return (pld);
+}
