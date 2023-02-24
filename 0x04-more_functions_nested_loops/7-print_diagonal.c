244515cb228dd9693c8891d5d34455c6e4b27be8
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-02-23 16:49:20 -0800

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a function that draws a diagonal
+ * @n: number of times
+ * Return: if n is 0 or less, print \n
+ */
+
+void print_diagonal(int n)
+{
+	int row, num_lines;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= n; row++)
+		{
+			if (row > 1)
+			{
+				for (num_lines = 1; num_lines <= row - 1; num_lines++)
+				{
+				_putchar(' ');
+				}
+			}
+						_putchar('\\');
+						_putchar('\n');
+		}
+	}
+}
