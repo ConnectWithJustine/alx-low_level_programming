1e11050bce6651e4da6ea3382f485936c3b1062c
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-03-01 04:46:07 -0800

    adding strcat file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * @src: string to be conctenated
+ * @dest: string with concatenation
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int lengthD, lengthS;
+
+	lengthD = 0;
+	lengthS = 0;
+
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
+
+	while (*(src + lengthS) != '\0' && lengthD < 97)
+	{
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+		lengthS++;
+	}
+	*(dest + lengthD) = '\0';
+	return (dest);
+}
