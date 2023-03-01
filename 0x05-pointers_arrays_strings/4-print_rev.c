8b7958ac0398c387fdc7dc68f4174649c4c74445
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-02-28 20:25:44 -0800

    adding task 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#inlcude "main.h"
+/**
+ * print_rev - function that print a string, in reverse
+ * @s: string to be resereve
+ * Return: Always 0
+ */
+void print_rev(*s)
+{
+	int len = 0, i;
+
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
