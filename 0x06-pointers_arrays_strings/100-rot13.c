bef7e016fe9eb2dbf6a63821e9a55322cca39d83
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-03-01 05:44:55 -0800

    Adding file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - function that encodes a string using rot13
+ * @s: analized string.
+ *
+ * Return: String with all letters in ROT13 base.
+ */
+char *rot13(char *s)
+{
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j;
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (*(s + i) == a[j])
+			{
+				*(s + i) = rot[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
