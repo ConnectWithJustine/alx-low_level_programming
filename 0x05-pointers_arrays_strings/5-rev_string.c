a7e63de4f0eddad1eb28943b6e4898034a83c4e1
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-02-28 20:42:53 -0800

    adding rev string file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - prints in reverse a string.
+ * @s: tested string
+ * Return: always 0.
+ */
+void rev_string(char *s)
+{
+	int length, j;
+	char a;
+
+	length = 0;
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+	for (j = 0; j < (length / 2); j++)
+	{
+		a = *(s + j);
+		*(s + j) = *(s + length - j - 1);
+		*(s + length - j - 1) = a;
+	}
+}
