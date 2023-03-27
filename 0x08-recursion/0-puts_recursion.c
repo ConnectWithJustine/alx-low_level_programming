9c62c75e57b11e47940d60a77d38f78b6dd712f7
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-03-27 16:49:20 -0700

    correcting puts_resursion file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,16 +5,16 @@
  *
  * Return: Always 0
  */
-
-void _puts_rescursion(char *s)
+void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-	{
-		_putchar('\n');
-	}
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
 }
