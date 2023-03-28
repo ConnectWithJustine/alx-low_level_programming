a4b6564e6d294043125d120fd735143e4277185e
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-03-27 17:46:39 -0700

    adding power file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - a function that returns the value of x raised to power y
+ * @x: int
+ * @y: int
+ * Return: -1 if y < 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y != 0)
+		return (x * _pow_recursion(s, y - 1));
+
+	else
+	{
+		return (1);
+	}
+}
