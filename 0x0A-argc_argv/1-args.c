2f2f23893e59786ea73002cf3dd1dab4b76b2599
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-03-31 19:33:58 -0700

    adding 1-args file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - program that prints the number of arguments passed into it
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
