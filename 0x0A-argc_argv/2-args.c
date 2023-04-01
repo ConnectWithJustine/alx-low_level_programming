5ed61ed9d4b3e6d8bdf5eb612a3964d29d7b857a
Ugwu Justine Obinna <ugwujustineobinna@gmail.com>
2023-03-31 20:03:48 -0700

    adding 2-args file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - a program that prints all arguments it receives
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int j;
+
+	for (j = 0; j < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
+
