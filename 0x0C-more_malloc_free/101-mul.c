32b292e9e3500af65a1ad42b6d931d84ce9f7baa
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-06-15 18:28:07 -0700

    mul file update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,26 +5,27 @@
  *
  * Return: 0 on success else 1
  */
-
-int is_positive_number(char* str) 
+int is_positive_number(char *str)
 {
-    int i = 0;
-    while (str[i]) 
-    {
-        if (str[i] < '0' || str > '9')
-            return 0;
-        i++;
-    }
-    return 1;
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
  * print_error_exit - a function that checkof error
  * Exit: 98
  */
-void print_error_exit() {
-    printf("Error\n");
-    exit(98);
+void print_error_exit(void)
+{
+	printf("Error\n");
+	exit(98);
 }
 
 /**
@@ -33,20 +34,16 @@ void print_error_exit() {
  * @argv: array of pointers to argument strings
  * Return: always 0
  */
-int main(int argc, char* argv[]) {
-    if (argc != 3)
-        print_error_exit();
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		print_error_exit();
+	if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
+		print_error_exit();
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
 
-    if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
-        print_error_exit();
-
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-
-    int result = num1 * num2;
-
-    printf("%d\n", result);
-
-    return 0;
+	printf("%d\n", result);
+	return (0);
 }
-
