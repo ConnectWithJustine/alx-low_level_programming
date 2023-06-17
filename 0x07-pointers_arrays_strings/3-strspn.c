7d8224bf5f58d5e82c419f76d86a4b23f1108dea
Ugwu Justine <87438032+ugwujustine@users.noreply.github.com>
2023-06-17 22:04:39 +0100

    Update file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,21 +9,18 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int cont = 0;
-	int found;
 
 	for (unsigned int i = 0; s[i] != '\0'; i++)
 	{
-		found = 0;
 		for (unsigned int j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				cont++;
-				found = 1;
 				break;
 			}
 		}
-		if (found == 0)
+		if (s[i] != accept[j])
 			break;
 	}
 	return (cont);
