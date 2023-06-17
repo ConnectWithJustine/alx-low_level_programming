2b40f3eb6fdb8f1b8905cdc46dff6aba1119ce11
Ugwu Justine <87438032+ugwujustine@users.noreply.github.com>
2023-06-17 23:14:54 +0100

    Update file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,16 +9,21 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
+	char *ptr = NULL;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
+		ptr = &haystack[i];
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i + j] != needle[j])
+			{
+				ptr = NULL;
 				break;
+			}
 		}
 		if (needle[j] == '\0')
-			return (&haystack[i]);
+			return (ptr);
 	}
 
 	return (NULL);
