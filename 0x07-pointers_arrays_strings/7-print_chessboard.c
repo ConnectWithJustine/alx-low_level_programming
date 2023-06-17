6eef6bcd1f4e31bc08f2f8e4adbd11b494f8ffe9
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-06-17 15:48:52 -0700

    solve task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_chessboard - a function that prints the chessboard.
+ * @a: 2d array
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
