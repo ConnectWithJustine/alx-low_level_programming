85fcb89b8f4943d3bc482bf9fe534e7896d80afd
Ugwu Justine <87438032+ugwujustine@users.noreply.github.com>
2023-06-17 05:26:59 +0100

    Update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	if (i = 0; i < n; i++)
+	while (i < n)
 	{
 		*(s + i) = b;
+		i++;
 	}
 	return (s);
 }
