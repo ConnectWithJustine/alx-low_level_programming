a08f6adfb7498cc64f3097112a624f10562efaea
Ugwu Justine <87438032+ugwujustine@users.noreply.github.com>
2023-06-17 23:39:12 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,34 +3,27 @@
  * _strstr - a function that locates a substring.
  * @haystack: string
  * @needle: substring
- *
+ * fork from max git repo
  * Return: pointer to the beginning of the located substring
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, exit;
+	int i = 0, j = 0, exit;
 	char *ptr;
 
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		exit = 0;
-		for (j = 0; accept[j] != '\0'; j++)
+start_from_here:j = 0;
+	exit = 0;
+	for (; (haystack[i] != '\0'); i++)
+		if (haystack[i] == needle[j])
 		{
-			if (haystack[i] == needle[j])
-			{
-				ptr = &haystack[i];
-				exit = 1;
-				break;
-			}
-		}
-		if (exit)
+			ptr = &haystack[i];
 			break;
-	}
-
+		}
 	if (haystack[i] == '\0')
-		return (NULL);
-
-	while (haystack[i] != '\0' && needle[j] != '\0')
+	{
+		return ('\0');
+	}
+	while ((haystack[i] != '\0') && (needle[j] != '\0'))
 	{
 		if (haystack[i] != needle[j])
 		{
@@ -40,12 +33,12 @@ char *_strstr(char *haystack, char *needle)
 		i++;
 		j++;
 	}
-
 	if (needle[j] == '\0' && !exit)
+	{
 		return (ptr);
-
+	}
 	if (haystack[i] == '\0')
-		return (NULL);
-
+		return (0);
+	goto start_from_here;
 	return (ptr);
 }
