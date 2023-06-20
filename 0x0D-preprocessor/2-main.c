453fb2abe06f413b82f1e30d8f9f37d2d985a994
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-06-20 15:01:22 -0700

    fix code

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,13 @@
-#include <stdio.h>_
-/**
- * main - prints the name of the file
- * Return: Nothing
- */
+#include <stdio.h>
 
+#define NAME __FILE__
+
+/**
+ * main - Prints name of file
+ * Return: Always 0(success)
+ */
 int main(void)
 {
-	printf("%s\n", __FILE__);
+	printf("%s\n", NAME);
 	return (0);
 }
