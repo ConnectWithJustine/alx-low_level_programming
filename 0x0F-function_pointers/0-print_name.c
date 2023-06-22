527c8ada545a8b2b1bd4d897e4734d30b8a078b6
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-06-21 19:55:17 -0700

    include function name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
-#include "func.h"
-#include <stdlib.h>
+#include "function_pointers.h"
 /**
  * print_name - a function that prints a name.
  * @name: string
@@ -9,7 +8,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
 	f(name);
 }
