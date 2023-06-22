890345087e10d53c671678fb543182c29e85a5b3
Ugwu Justine <87438032+ugwujustine@users.noreply.github.com>
2023-06-22 03:59:53 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - a function that prints a name.
  * @name: string
@@ -8,7 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
