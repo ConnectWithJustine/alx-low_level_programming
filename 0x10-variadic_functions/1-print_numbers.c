17f52e80492a3d06358161eaffe1ebf5fc3d323a
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-06-22 20:26:10 -0700

    use _putchar

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,23 +1,54 @@
 #include "variadic_functions.h"
+
 /**
  * print_numbers - a function that prints numbers, followed by a new line.
- * @separator: string to be printed between numbers
- * @n: number of integers passed to the function
- * Return: separator is NULL, don’t print it
+ * @separator: the string to be printed between numbers.
+ * @n: the number of integers passed to the function.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list vlist;
 	unsigned int i;
+	int num;
 
 	va_start(vlist, n);
-
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(vlist, n));
-	if (separator != NULL && i < n - 1)
-		printf("%s", separator);
+		num = va_arg(vlist, int);
+		print_number(num);
+
+		if (separator != NULL && i != n - 1)
+			print_separator(separator);
 	}
-	printf("\n");
-	va_end(vlist);
+	va_end(args);
+	_putchar('\n');
+}
+
+/**
+ * print_number - prints a number using _putchar.
+ * @num: the number to be printed.
+ */
+void print_number(int num)
+{
+	if (num < 0)
+	{
+		_putchar('-');
+		num *= -1;
+	}
+	if (num / 10)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
+
+/**
+ * print_separator - Prints a separator string using _putchar.
+ * @separator: The separator string to be printed.
+ */
+void print_separator(const char *separator)
+{
+	while (*separator)
+	{
+		_putchar(*separator);
+		separator++;
+	}
 }
