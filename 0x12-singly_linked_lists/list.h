ba8ab2fe0157113ef3bc7ac0bab78601bbc5b4e8
Ugwu Justine <87438032+ugwujustine@users.noreply.github.com>
2023-07-03 23:51:56 +0100

    Update list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -8,14 +8,14 @@ int _putchar(char c);
 /**
  * struct list_s - singly linked list
  * @str: string
- * @length: length of the string
+ * @len: length of the string
  * @next: points to the next node
  *
  */
 typedef struct list_s
 {
 	char *str;
-	unsigned int length;
+	unsigned int len;
 	struct list_s *next;
 } list_t;
 
