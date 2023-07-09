4633f1be4f1b9f724f5e867707bb32fb7b3d1e89
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-07-08 20:35:25 -0700

    update clear bit file

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,11 +9,10 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
-		return (-1);
-	unsigned long int mask = 1UL << index;
-
-	mask = ~mask;
-	*n &= mask;
-	return (1);
+	if (index < 64)
+	{
+		*n = *n & ~(1 << index);
+		return (1);
+	}
+	return (-1);
 }
