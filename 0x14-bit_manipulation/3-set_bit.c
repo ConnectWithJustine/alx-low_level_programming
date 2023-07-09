aa969d85168fa5de7a98ab87316d1e7fcfb857bc
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-07-08 20:32:55 -0700

    update set bit file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,9 +7,9 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
-		return (-1);
-	unsigned long int mask = 1UL << index;
-	*n |= mask;
+	*n += 1 << index;
+	{
 	return (1);
+	}
+	return (-1);
 }
