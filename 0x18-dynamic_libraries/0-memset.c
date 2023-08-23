8346154011af39bfc2a26385831d2defb0f138b0
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-08-23 01:30:58 +0100

    fix error

--- 0x18-dynamic_libraries/0-memset.c
+++ 0x18-dynamic_libraries/0-memset.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - empty
- * @s: nothing
- * @b: nothing
- * @n: nothing
- * Return: nothing
+ * _memset - Fill memory with a constant byte
+ * @s: Pointer to the memory to be filled
+ * @b: The byte value to fill the memory with
+ * @n: The number of bytes to fill
+ * Return: Pointer to the memory area `s`
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	_memset(*s, b, n);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -1,13 +1,31 @@
 #include "main.h"
 
 /**
- * _strcat - empty
- * @dest: nothing
- * @src: nothing
- * Return: nothing
+ * _strcat - Concatenate two strings
+ * @dest: Destination string (will contain the concatenated result)
+ * @src: Source string (will be appended to the destination)
+ * Return: Pointer to the destination string `dest`
  */
 
 char *_strcat(char *dest, char *src)
 {
-	_strcat(*dest, *src);
+	int i, dest_len = 0;
+
+	/* Find the length of the destination string */
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	/* Append the source string to the destination string */
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+
+	/* Null-terminate the concatenated string */
+	dest[dest_len + i] = '\0';
+	return (dest);
 }
